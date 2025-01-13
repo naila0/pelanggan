02d07661a053fcdb0788dd52e112f7455e6fdb7a
naila0 <nailaf677@gmail.com>
2025-01-13 11:38:56 +0700

    menambah fungsi ubah pelanggan

--- main.js
+++ main.js
@@ -46,19 +46,36 @@ export async function ambilDaftarPelanggan() {
 
   return hasilKueri;
 }
-export async function tambahInventory(nama, alamat, nohape) {
+// fungsi menambah data pelanggan
+export async function tambahPelanggan(nama, alamat, nohape) {
   try {
     // menyimpan data ke firebase
     const refDokumen = await addDoc(collection(basisdata, "pelanggan"), {
-      nama: nama,
-      alamat: alamat,
-      nohape: nohape
+    nama: nama, 
+    alamat: alamat,
+    nohape: nohape
     })
-
+    
     // menampilkan pesan berhasil
     console.log('berhasil menyimpan data pelanggan')
   } catch (error) {
     // menampilkan pesan gagal 
     console.log('gagal menyimpan data pelanggan')
   }
+}
+export async function hapusPelanggan(id) {
+  await deleteDoc(doc(basisdata, "pelanggan", id))
+}
+export async function ubahPelanggan(id, namabaru, alamatbaru, nohapebaru) {
+  await updateDoc(
+    doc(basisdata, "pelanggan", id), 
+    { nama: namabaru, alamat: alamatbaru, nohape: nohapebaru }
+    )
+}
+  
+export async function ambilPelanggan(id) {
+  const refDokumen = await doc(basisdata, "pelanggan", id)
+  const snapshotDokumen = await getDoc(refDokumen)
+
+  return await snapshotDokumen.data()
 }
